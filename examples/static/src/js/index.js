58137b8b0aa05e2a968d23caa14d9bdea5b2c95e
JakeSidSmith <jake@dabapps.com>
2015-07-30 20:42:21 +0100

    Move existing examples to React js

--- examples/static/src/js/index.js
+++ examples/static/src/js/index.js
@@ -1,5 +1,44 @@
-'use strict';
+(function () {
+  'use strict';
 
-var Licons = require('../../../../src/js/index.js');
+  var React = require('react');
+  var Licon = require('../../../../src/js/index.js');
 
-console.log(Licons);
+  var App = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <p>
+            <Licon border />
+            <Licon border rounded />
+            <Licon border circle />
+            <Licon rounded />
+          </p>
+
+          <p>
+            <Licon icon='menu' large border />
+            <Licon icon='menu' large border rounded />
+            <Licon icon='menu' large border circle />
+            <Licon icon='menu' large rounded />
+          </p>
+
+          <p>
+            <Licon icon='menu' border />
+            <Licon icon='menu' border rounded />
+            <Licon icon='menu' border circle />
+            <Licon icon='menu' rounded />
+          </p>
+
+          <p>
+            <Licon icon='menu' small border />
+            <Licon icon='menu' small border rounded />
+            <Licon icon='menu' small border circle />
+            <Licon icon='menu' small rounded />
+          </p>
+        </div>
+      );
+    }
+  });
+
+  React.render(<App />, document.body);
+})();
