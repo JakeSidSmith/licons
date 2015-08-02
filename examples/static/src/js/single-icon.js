e421ea54e14e2f72e223b93cf575382d7217779f
JakeSidSmith <jake@dabapps.com>
2015-08-02 16:43:02 +0100

    Toggle border

--- examples/static/src/js/single-icon.js
+++ examples/static/src/js/single-icon.js
@@ -27,15 +27,44 @@
       setInterval(this.selectRandom, 2000);
     },
 
+    setBorder: function (value) {
+      this.setState({
+        border: value
+      });
+    },
+
     getInitialState: function () {
       return {
-        iconIndex: 0
+        iconIndex: 0,
+        border: true
       };
     },
 
     render: function () {
       return (
-        <Licon icon={this.props.icons[this.state.iconIndex]} border round />
+        <div className='row single-icon'>
+          <div className='col-xs-12 col-sm-6 col-md-4 col-sm-push-3 col-md-push-4'>
+            <Licon icon='arrow-left' className='pull-left' />
+            <Licon icon={this.props.icons[this.state.iconIndex]} border={this.state.border} round />
+            <Licon icon='arrow-right' className='pull-right' />
+            <p>
+              {this.props.icons[this.state.iconIndex]}
+            </p>
+            <div className='form-inline'>
+              <div className='form-group'>
+                <label>Border:</label>
+              </div>
+              <div className='form-group'>
+                <label>On</label>
+                <input type='radio' checked={this.state.border} onChange={this.setBorder.bind(this, true)} />
+              </div>
+              <div className='form-group'>
+                <label>Off</label>
+                <input type='radio' checked={!this.state.border} onChange={this.setBorder.bind(this, false)} />
+              </div>
+            </div>
+          </div>
+        </div>
       );
     }
   });
